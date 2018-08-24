3e5404eeb0ad0c811f19c9165d34eaf180baaccc
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-08-24 03:54:23 -0400

    Added army data

--- db/migrate/20180824060239_create_units.rb
+++ db/migrate/20180824060239_create_units.rb
@@ -2,8 +2,6 @@ class CreateUnits < ActiveRecord::Migration[5.2]
   def change
     create_table :units do |t|
       t.string :name, null: false
-      t.string :size
-      t.string :special
       t.belongs_to :army
 
       t.timestamps null: false
